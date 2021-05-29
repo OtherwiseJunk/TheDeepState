64dfe5588bf09b8530288da6a9aece735a4453b6
Bobby David <RobertWarrenDavid@gmail.com>
2021-05-29 15:52:35 -0400

    Add service and models

---
+++ DeepState.Data/Context/GuildUserRecordContext.cs
@@ -0,0 +1,57 @@
+﻿using DeepState.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DeepState.Data.Context
+{
+	class GuildUserRecordContext : DbContext
+	{
+		public GuildUserRecordContext(DbContextOptions options) : base(options)
+		{
+		}
+
+		public const decimal SMALLEST_PAYOUT = 0.00000001m;
+		public const decimal LARGEST_PAYOUT = 0.00000100m;
+
+		public DbSet<UserRecord> UserRecords { get; set; }
+
+		public bool UserRecordExists(ulong userId, ulong guildId)
+		{
+			return UserRecords.FirstOrDefaultAsync(ur => ur.DiscordUserId == userId && ur.DiscordGuildId == guildId) != null;
+		}
+
+		public void IssuePayout(ulong userId, ulong guildId)
+		{
+			decimal payoutAmount = RollPayout();
+			if (UserRecordExists(userId, guildId))
+			{
+				UserRecords.FirstAsync(ur => ur.DiscordUserId == userId && ur.DiscordGuildId == guildId)
+					.Result.LibcraftCoinBalance += payoutAmount;
+			}
+			else
+			{
+				UserRecords.Add(new UserRecord
+				{
+					DiscordUserId = userId,
+					DiscordGuildId = guildId,
+					LibcraftCoinBalance = payoutAmount,
+					TimeOut = false
+				}) ;
+			}
+		}
+		public decimal GetUserBalance(ulong userId, ulong guildId)
+		{
+			return UserRecords.FirstAsync(ur => ur.DiscordUserId == userId && ur.DiscordGuildId == guildId)
+					.Result.LibcraftCoinBalance;
+		}
+
+		public decimal RollPayout()
+		{
+			Random rand = new Random(DateTime.Now.Millisecond * DateTime.Now.Second);
+			decimal roll = new decimal(rand.NextDouble() + 0.01) * LARGEST_PAYOUT;
+			return roll >= SMALLEST_PAYOUT ? roll : SMALLEST_PAYOUT;
+		}
+	}
+}
---
+++ DeepState.Data/Models/UserRecord.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace DeepState.Data.Models
+{
+	public class UserRecord
+	{
+		[Key]
+		public int UserId { get; set; }
+		public ulong DiscordUserId { get; set; }
+		public ulong DiscordGuildId { get; set; }
+		public decimal LibcraftCoinBalance { get; set; }
+		public int TableFlipCount { get; set; }
+		public bool TimeOut { get; set; }
+	}
+}
