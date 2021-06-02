c25a990bd9d00bbfd31edc834ad42b6ef3a0409e
dninemfive <11084010+dninemfive@users.noreply.github.com>
2021-06-02 10:25:31 -0700

    fix regex

--- DeepState/Constants/SharedConstants.cs
+++ DeepState/Constants/SharedConstants.cs
@@ -92,7 +92,7 @@ namespace DeepState.Constants
 
 		#region Strings
 		public static string KlaxonResponse = "https://www.youtube.com/watch?v=xU5mOT57ghM";
-		public static string SusRegex = ".*[(sus)(amo.*gus)].*";
+		public static string SusRegex = "(sus[^\w]|amogus|among us)";
 		#endregion
 
 		#region String Formats
