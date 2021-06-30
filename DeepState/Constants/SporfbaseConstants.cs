515c59182b370622387f06886012d0459ae98e3e
Bobby <RobertWarrenDavid@gmail.com>
2021-06-30 16:51:44 -0400

    Update SporfbaseConstants.cs

--- DeepState/Constants/SporfbaseConstants.cs
+++ DeepState/Constants/SporfbaseConstants.cs
@@ -2,7 +2,7 @@
 {
 	public class SporfbaseConstants
 	{
-		public const string ServerAddress { get; set; } = "sporfbase.com";
-		public const ushort ServerPort { get; set; } = 25565;
+		public const string ServerAddress = "sporfbase.com";
+		public const ushort ServerPort = 25565;
 	}
 }
