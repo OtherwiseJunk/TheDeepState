3e33745315671e48a736846248a242788ab1eac2
Bobby David <RobertWarrenDavid@gmail.com>
2022-02-14 18:51:45 -0500

    fix items maybe

---
+++ DeepState.Data/Migrations/RPG/20220214234842_FixItems.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DeepState.Data.Migrations.RPG
+{
+    public partial class FixItems : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Items_Characters_CharacterDiscordUserId",
+                table: "Items");
+
+            migrationBuilder.RenameColumn(
+                name: "CharacterDiscordUserId",
+                table: "Items",
+                newName: "characterDiscordUserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Items_CharacterDiscordUserId",
+                table: "Items",
+                newName: "IX_Items_characterDiscordUserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Items_Characters_characterDiscordUserId",
+                table: "Items",
+                column: "characterDiscordUserId",
+                principalTable: "Characters",
+                principalColumn: "DiscordUserId",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Items_Characters_characterDiscordUserId",
+                table: "Items");
+
+            migrationBuilder.RenameColumn(
+                name: "characterDiscordUserId",
+                table: "Items",
+                newName: "CharacterDiscordUserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Items_characterDiscordUserId",
+                table: "Items",
+                newName: "IX_Items_CharacterDiscordUserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Items_Characters_CharacterDiscordUserId",
+                table: "Items",
+                column: "CharacterDiscordUserId",
+                principalTable: "Characters",
+                principalColumn: "DiscordUserId",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
--- DeepState.Data/Models/RPGModels/Item.cs
+++ DeepState.Data/Models/RPGModels/Item.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,11 @@ namespace DeepState.Data.Models.RPGModels
 {
 	public class Item
 	{
+		[Key]
 		public int ItemID { get; set; }
 		public string Name { get; set; }
 		public string Description { get; set; }
 		public int Price { get; set; }
+		public Character character { get; set; }
 	}
 }
