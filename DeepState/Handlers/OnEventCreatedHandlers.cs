952bb19228cfe3656be75113da7d15b713233c1a
Bobby David <RobertWarrenDavid@gmail.com>
2022-04-23 18:40:42 -0400

    fix events

--- DeepState/Handlers/OnEventCreatedHandlers.cs
+++ DeepState/Handlers/OnEventCreatedHandlers.cs
@@ -11,26 +11,14 @@ namespace DeepState.Handlers
 {
     internal class OnEventCreatedHandlers
     {
+        public static string EventUrlFormat = @"https://discord.com/events/{0}/{1}";
         public static void AnnounceNewEvent(SocketGuildEvent arg)
         {
             ITextChannel announcementChnl = (ITextChannel) arg.Guild.Channels.FirstOrDefault(c => c.Name.ToLower() == "announcements");
             if(announcementChnl != null)
-            {
-                string creator = BotUtilities.GetDisplayNameForUser(arg.Creator);
-                EmbedBuilder eb = new();
-                eb.Title = $"{arg.Name} by {creator}";
-                eb.AddField("When", $"{arg.StartTime.ToString("f")}");
-                if (arg.Type == GuildScheduledEventType.External)
-                {
-                    eb.AddField("Where", $"{arg.Location.ToString()}");
-                }
-                else if (arg.Type == GuildScheduledEventType.Voice)
-                {
-                    eb.AddField("Where", $"{arg.Channel}");
-                }
-                eb.AddField("Why", $"{arg.Description}");                
-                if(arg.CoverImageId != null) eb.ImageUrl = arg.GetCoverImageUrl();
-                announcementChnl.SendMessageAsync($"{creator} has created a new event!", embed: eb.Build());
+            {                
+                string eventMessage = $"{BotUtilities.GetDisplayNameForUser(arg.Creator)} has created a new event!{Environment.NewLine}{Environment.NewLine}{String.Format(EventUrlFormat, arg.Guild.Id, arg.Id)}";
+                announcementChnl.SendMessageAsync(eventMessage);
             }
         }
     }
