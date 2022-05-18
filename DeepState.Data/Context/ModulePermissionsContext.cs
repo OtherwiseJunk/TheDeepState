13a161ac721776b81c5fa06cd9501026be3b52aa
Bobby David <RobertWarrenDavid@gmail.com>
2022-05-17 20:34:23 -0400

    Add start of module permission checks

---
+++ DeepState.Data/Context/ModulePermissionsContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using DeepState.Data.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeepState.Data.Context
+{
+    public class ModulePermissionsContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(Environment.GetEnvironmentVariable("DATABASE"));
+        }
+        public DbSet<GuildModulePermissions> GuildModulePermissions { get; set; }
+        public DbSet<ChannelModulePermissions> ChannelModulePermissions { get; set; }
+    }
+}
---
+++ DeepState.Data/Models/GuildModulePermissions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeepState.Data.Models
+{
+    public class GuildModulePermissions
+    {
+        [Key]
+        public ulong GuildId { get; set; }
+        public bool HungerGamesModule { get; set; }
+        public bool JackboxModule { get; set; }
+        public bool MalarkeyModule { get; set; }
+        public bool ModTeamRequestsModule { get; set; }
+        public bool OutOfContextModule { get; set; }
+        public bool RPGModule { get; set; }
+        public bool UserRecordModule { get; set; }
+    }
+}
