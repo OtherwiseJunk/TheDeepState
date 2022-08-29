de168b4a0a185959b34eafc3c49a2a60b95b15e0
Bobby David <RobertWarrenDavid@gmail.com>
2022-08-29 13:33:33 -0400

    lol

--- DeepState/Modules/FeedbackModule.cs
+++ DeepState/Modules/FeedbackModule.cs
@@ -4,7 +4,6 @@ using Discord.Commands;
 using System.Linq;
 using System.Threading.Tasks;
 using Serilog;
-using System;
 
 namespace DeepState.Modules
 {
@@ -14,7 +13,7 @@ namespace DeepState.Modules
         ILogger _log { get; set; }
         PanopticonService _service { get; set; }
         ulong FeedbackChannelId = 967308491041693706;
-        ulong LibcraftGuildId = 698639095940907048;
+        ulong LibcraftGuildId = 698639095940907048;        
 
         public FeedbackModule(PanopticonService service, ILogger logger)
         {
@@ -26,54 +25,32 @@ namespace DeepState.Modules
         [Summary("Sends feedback to the Libcraft Admins")]
         public async Task CreateFeedback([Remainder] string feedback)
         {
-            try
+            _log.Information("Received a feedback command");
+            IGuild libcraftGuild = Context.Client.GetGuildAsync(LibcraftGuildId).Result;
+            if(libcraftGuild != null)
             {
-                _log.Information("Received a feedback command");
-
-                IGuild libcraftGuild = null;
-                foreach(IGuild guild in Context.Client.GetGuildsAsync().Result)
+                ITextChannel feedbackChannel = (ITextChannel)libcraftGuild.GetChannelAsync(FeedbackChannelId).Result;
+                if (feedbackChannel == null)
                 {
-                    Console.WriteLine($"Does guild ID match? {guild.Id} == {LibcraftGuildId} = {guild.Id == LibcraftGuildId}");
-                    if(guild.Id == LibcraftGuildId)
-                    {
-                        libcraftGuild = guild;
-                    }
-                }
-                if (libcraftGuild == null)
-                {
-                    ITextChannel feedbackChannel = (ITextChannel)libcraftGuild.GetChannelAsync(FeedbackChannelId).Result;
-                    if (feedbackChannel == null)
-                    {
-                        _log.Information("Attempting to create feedback.");
-                        _service.CreateFeedback(feedback, Context.Message.Author.Id);
-                        _log.Information("Created feedback");
-                        int id = _service.GetAllFeedback().First(fb => fb.Message == feedback).Id;
-                        _log.Information("Extracted ID");
-                        EmbedBuilder builder = new EmbedBuilder();
-                        builder.Title = $"Feedback #{id}";
-                        builder.Description = feedback;
-                        await feedbackChannel.SendMessageAsync(embed: builder.Build());
-                        _log.Information("Feedback sent to reports channel.");
-                    }
-                    else
-                    {
-                        _log.Error("Failed to find libcraft report channel when attempting to create feedback");
-                    }
+                    _log.Information("Attempting to create feedback.");
+                    _service.CreateFeedback(feedback, Context.Message.Author.Id);
+                    _log.Information("Created feedback");
+                    int id = _service.GetAllFeedback().First(fb => fb.Message == feedback).Id;
+                    _log.Information("Extracted ID");
+                    EmbedBuilder builder = new EmbedBuilder();
+                    builder.Title = $"Feedback #{id}";
+                    builder.Description = feedback;
+                    await feedbackChannel.SendMessageAsync(embed: builder.Build());
+                    _log.Information("Feedback sent to reports channel.");
                 }
                 else
                 {
-                    _log.Error("Failed to find libcraft Guild when attempting to create feedback");
-                }
-            }    
-            catch(Exception ex)
+                    _log.Error("Failed to find libcraft report channel when attempting to create feedback");
+                }                
+            }
+            else
             {
-                _log.Error(ex.Message);
-                _log.Information("Looping through any inner exceptions...");
-                while (ex.InnerException != null)
-                {
-                    _log.Error(ex.InnerException.Message);
-                    ex = ex.InnerException;
-                }
+                _log.Error("Failed to find libcraft Guild when attempting to create feedback");
             }
         }
     }
