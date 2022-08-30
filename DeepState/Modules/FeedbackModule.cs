adafd4c3224c68284b8af6ae4b800bedfaeac9aa
Bobby David <RobertWarrenDavid@gmail.com>
2022-08-29 21:28:14 -0400

    add getAllFeedback method

--- DeepState/Modules/FeedbackModule.cs
+++ DeepState/Modules/FeedbackModule.cs
@@ -7,6 +7,9 @@ using Serilog;
 using DartsDiscordBots.Services;
 using System.Collections.Generic;
 using System;
+using Panopticon.Shared.Models;
+using DartsDiscordBots.Permissions;
+using DeepState.Constants;
 
 namespace DeepState.Modules
 {
@@ -17,7 +20,7 @@ namespace DeepState.Modules
         PanopticonService _panopticon { get; set; }
         ImagingService _imaging { get; set; }
         ulong FeedbackChannelId = 967308491041693706;
-        ulong LibcraftGuildId = 698639095940907048;        
+        ulong LibcraftGuildId = 698639095940907048;
 
         public FeedbackModule(PanopticonService service, ImagingService imaging, ILogger logger)
         {
@@ -31,40 +34,67 @@ namespace DeepState.Modules
         public async Task CreateFeedback([Remainder] string feedback)
         {
             _log.Information("Received a feedback command");
-            IGuild libcraftGuild = Context.Client.GetGuildAsync(LibcraftGuildId).Result;
-            if(libcraftGuild != null)
+            ITextChannel feedbackChannel = GetFeedbackChannel();
+
+            if (feedbackChannel != null)
             {
-                ITextChannel feedbackChannel = (ITextChannel)libcraftGuild.GetChannelAsync(FeedbackChannelId).Result;
-                if (feedbackChannel != null)
-                {
-                    feedback += GenerateAttachmentLinksString(Context.Message.Attachments);
-                    _log.Information("Attempting to create feedback.");
-                    _panopticon.CreateFeedback(feedback, Context.Message.Author.Id);
-                    _log.Information("Created feedback");
-                    int id = _panopticon.GetAllFeedback().First(fb => fb.Message == feedback.Replace(Environment.NewLine, String.Empty)).Id;
-                    _log.Information("Extracted ID");
-                    EmbedBuilder builder = new EmbedBuilder();
-                    builder.Title = $"Feedback #{id}";
-                    builder.Description = feedback;
-                    await feedbackChannel.SendMessageAsync(embed: builder.Build());
-                    await Context.Message.Author.SendMessageAsync("Successfully submitted your feedback to the Libcraft team.");
-                    _log.Information("Feedback sent to reports channel.");
-                }
-                else
-                {
-                    _log.Error("Failed to find libcraft report channel when attempting to create feedback");
-                }                
+                feedback += GenerateAttachmentLinksString(Context.Message.Attachments);
+                _log.Information("Attempting to create feedback.");
+                _panopticon.CreateFeedback(feedback, Context.Message.Author.Id);
+                _log.Information("Created feedback");
+                int id = _panopticon.GetAllFeedback().First(fb => fb.Message == feedback.Replace(Environment.NewLine, String.Empty)).Id;
+                _log.Information("Extracted ID");
+                EmbedBuilder builder = new EmbedBuilder();
+                builder.Title = $"Feedback #{id}";
+                builder.Description = feedback;
+                await feedbackChannel.SendMessageAsync(embed: builder.Build());
+                await Context.Message.Author.SendMessageAsync("Successfully submitted your feedback to the Libcraft team.");
+                _log.Information("Feedback sent to reports channel.");
             }
             else
             {
-                _log.Error("Failed to find libcraft Guild when attempting to create feedback");
+                _log.Error("Failed to find libcraft report channel when attempting to create feedback");
             }
         }
 
+        [Command("list")]
+        [Summary("Gets a list of all feedback. Only works for admins")]
+        [RequireOwner(Group = "admins"), RequireUserPermission(GuildPermission.ManageGuild), RequireGuild(new ulong[] { SharedConstants.LibcraftGuildId })]
+        public async Task GetListOfFeedback()
+        {
+            List<Feedback> feedbackList = _panopticon.GetAllFeedback();
+            if (feedbackList != null)
+            {
+                if (feedbackList.Count > 0)
+                {
+                    EmbedBuilder builder = new EmbedBuilder();
+                    builder.Title = "Libcraft Feedback Submissions";
+                    foreach (Feedback feedback in feedbackList)
+                    {
+                        builder.AddField($"{feedback.Id}", feedback.Message);
+                    }
+                    ITextChannel feedbackChannel = GetFeedbackChannel();
+                    _ = feedbackChannel.SendMessageAsync(embed: builder.Build());
+                }
+                else
+                {
+                    _ = Context.Channel.SendMessageAsync("Sorry, looks like there's no active feedback submissions");
+                }
+            }
+            else
+            {
+                _ = Context.Channel.SendMessageAsync("Shit Broke :crying:");
+            }
+        }
+        private ITextChannel GetFeedbackChannel()
+        {
+            IGuild libcraftGuild = Context.Client.GetGuildAsync(LibcraftGuildId).Result;
+            return (ITextChannel)libcraftGuild.GetChannelAsync(FeedbackChannelId);
+        }
         private string GenerateAttachmentLinksString(IReadOnlyCollection<IAttachment> attachments)
         {
             string links = $"{Environment.NewLine}Attachments:{Environment.NewLine}";
-            if(attachments.Count == 0)
+            if (attachments.Count == 0)
             {
                 _log.Information("Found no attachments on message");
                 return string.Empty;
