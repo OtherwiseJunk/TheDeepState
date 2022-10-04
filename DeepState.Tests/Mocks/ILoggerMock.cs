f661f0e5a8ac49fa442388a9c123f5e8333798ce
Bobby David <RobertWarrenDavid@gmail.com>
2022-10-04 16:07:07 -0400

    Modify ILoggerMock to log messages Verify log count in tests

--- DeepState.Tests/Mocks/ILoggerMock.cs
+++ DeepState.Tests/Mocks/ILoggerMock.cs
@@ -1,14 +1,17 @@
-﻿using Serilog;
+﻿using NUnit.Framework;
+using Serilog;
 using Serilog.Events;
-using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace DeepState.Tests.Mocks
 {
     internal class ILoggerMock : ILogger
     {
+        public List<LogEvent> events { get; set; } = new List<LogEvent>();
         public void Write(LogEvent logEvent)
         {
-            Console.WriteLine("log");
+            events.Add(logEvent);
         }
     }
 }
