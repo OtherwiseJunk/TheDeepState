47eea0d8838642d2e1ebffb5708199f8e155554f
Wren <otherwisejunk@gmail.com>
2023-01-27 11:49:52 -0500

    make async again.

--- DeepState/Program.cs
+++ DeepState/Program.cs
@@ -57,19 +57,19 @@ namespace DeepState
             JobManager.Initialize();
             JobManager.AddJob(() => HungerGameUtilities.DailyEvent((HungerGamesService)_services.GetService(typeof(HungerGamesService)), (UserRecordsService)_services.GetService(typeof(UserRecordsService)), _client), s => s.ToRunEvery(1).Days().At(8, 0));
             JobManager.AddJob(() => ((RPGService)_services.GetService(typeof(RPGService))).LongRest(), s => s.ToRunEvery(1).Days().At(8, 0));
-            JobManager.AddJob(() =>
+            JobManager.AddJob(async () =>
             {
                 Console.WriteLine("Attempting an automatic OOC post.");
                 OOCService ooc = _services.GetService(typeof(OOCService)) as OOCService;
                 Console.WriteLine($"OOC Service null? {ooc == null}");
                 IGuild libcraft = _client.GetGuild(SharedConstants.LibcraftGuildId);
-                IMessageChannel oocChannel = libcraft.GetChannelAsync(SharedConstants.LibcraftOutOfContext) as IMessageChannel;
+                IMessageChannel oocChannel = await libcraft.GetChannelAsync(SharedConstants.LibcraftOutOfContext) as IMessageChannel;
                 Console.WriteLine($"Channel null? {oocChannel == null}");
                 EmbedBuilder embed = ooc.BuildOOCEmbed(libcraft, oocChannel, ooc.GetRandomRecord());
                 Console.WriteLine($"Embed null? {embed == null}");
 
                 _ = oocChannel.SendMessageAsync("Heard from a reliable source that you're jonesing for some OOC. I gotchu.", embed: embed.Build());
-            }, s => s.ToRunEvery(2).Hours() );
+            }, s => s.ToRunEvery(2).Hours().At(0) );
         }
         public static void Main(string[] args)
     => new Program().MainAsync().GetAwaiter().GetResult();
