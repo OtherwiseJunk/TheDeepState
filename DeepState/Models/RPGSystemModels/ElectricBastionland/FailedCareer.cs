d4654ea859e054c2c61685136198011e23368e72
Wren <OtherwiseJunk@gmail.com>
2023-06-10 13:47:50 -0400

    update EB character generation to include failed career data (from json).

---
+++ DeepState/Models/RPGSystemModels/ElectricBastionland/FailedCareer.cs
@@ -0,0 +1,26 @@
+﻿namespace DeepState.Models.RPGSystemModels.ElectricBastionland
+{
+    public class FailedCareer
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string YoungestPlayerDebtor { get; set; }
+        public string StartingGear { get; set; }
+        public string MoneyQuestion { get; set; }
+        public string[] MoneyAnswers { get; set; }
+        public string HPQuestion { get;set; }
+        public string[] HPAnswers { get; set; }
+
+        public FailedCareer(string name, string description, string youngestPlayerDebtor, string startingGear, string moneyQuestion, string[] moneyAnswers, string hpQuestion, string[] hpAnswers)
+        {
+            Name = name;
+            Description = description;
+            YoungestPlayerDebtor = youngestPlayerDebtor;
+            StartingGear = startingGear;
+            MoneyQuestion = moneyQuestion;
+            MoneyAnswers = moneyAnswers;
+            HPQuestion = hpQuestion;
+            HPAnswers = hpAnswers;
+        }
+    }
+}
--- DeepState/Modules/CharacterGeneratorModule.cs
+++ DeepState/Modules/CharacterGeneratorModule.cs
@@ -1,21 +1,30 @@
 ﻿using DartsDiscordBots.Utilities;
+using DeepState.Models.RPGSystemModels.ElectricBastionland;
 using DeepState.Service;
 using Discord;
 using Discord.Commands;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
+using System.Web.Helpers;
 
 namespace DeepState.Modules
 {
     [Group("chargen"), Alias("cg")]
     internal class CharacterGeneratorModule : ModuleBase
     {       
-        RandomCharacterImageService _imagingService;
+        private RandomCharacterImageService _imagingService;
+        private FailedCareer[] failedCareers;
         public CharacterGeneratorModule(RandomCharacterImageService randomCharacterImageService) {
             _imagingService = randomCharacterImageService;
+            using (StreamReader reader = new StreamReader("./FailedCareers.json"))
+            {
+                failedCareers = JsonSerializer.Deserialize < FailedCareer[]>(reader.ReadToEnd());
+            }
         }
 
         [Command("electricbastionland"), Alias("eb")]
@@ -27,7 +36,8 @@ namespace DeepState.Modules
             int cha = d6.Roll(3).Total;
             int hp = d6.Roll();
             int money = d6.Roll();
-            int failedCareer = new Dice(100).Roll();
+            int failedCareerRoll = new Dice(failedCareers.Length).Roll();
+            FailedCareer career = failedCareers[failedCareerRoll - 1];
 
             EmbedBuilder builder = new();
             builder.Title = "Your shiny new Electric Bastionland character";
@@ -38,7 +48,11 @@ namespace DeepState.Modules
             builder.AddField("Charisma", cha);
             builder.AddField("Cash", money);
             builder.AddField("Health", hp);
-            builder.AddField("Failed Career Code", failedCareer);
+            builder.AddField($"Failed Career - {career.Name}:", $"{career.Description}");
+            builder.AddField("If you are the youngest player the whole group is £10K in debt to:", career.YoungestPlayerDebtor);
+            builder.AddField("You Get:", career.StartingGear);
+            builder.AddField(career.MoneyQuestion, career.MoneyAnswers[money - 1]);
+            builder.AddField(career.HPQuestion, career.HPAnswers[hp - 1]);
 
             _ = Context.Message.ReplyAsync(embed: builder.Build());
         }
