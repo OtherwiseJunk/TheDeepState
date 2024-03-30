7a876096d90ff543b1b5273dde3a0759462c0fd3
Alexis Cooper <peco6819@colorado.edu>
2024-03-30 17:15:54 -0600

    Potentially finished EmojiMonetization module, ready for testing

--- DeepState/Constants/EmojiMonetizationConstants.cs
+++ DeepState/Constants/EmojiMonetizationConstants.cs
@@ -4,11 +4,25 @@ using System.Collections.Generic;
 using Discord.Commands;
 using Discord;
 
-namespace DeepState.Data.Constants
+namespace DeepState.Constants
 {
     public static class EmojiMonetizationConstants
     {
+		public static List<string> ThankYouMessages = new List<string>
+		{
+			"Thank you for supporting the LibCraft:registered: Admin Team!",
+			"It's support like yours that makes servers like ours possible.",
+			"Thank you, ~~loyal customer~~ beloved friend!",
+			"Thank you for your support! Please consider donating *real* money to our gracious admins as well.",
+			"The LibCraft:registered: Admin Team thanks you for ~~lining their pockets~~ supporting our community.",
+			"Athanky!",
+			"Free emojis were a ZIRP. Thank you for your understanding.",
+			"Thank you for your support! Any complaints are to be addressed to our parent company, NotAShell Holdings, 1209 Orange Street, Wilmington, DE 19801."
+		};
+
 		// TODO: Finish populating this
+
+		/*
         public static List<Emote> BasicPack = new List<Emote>
 		{
 			Emote.Parse("<:emoji:707407221792702525>"),
@@ -19,7 +33,8 @@ namespace DeepState.Data.Constants
 			Emote.Parse("<:concernfroggy:737512429310836806>"),
 			Emote.Parse("<:gorepat:858524272258973696>"),
 			Emote.Parse("<:true:923420410350039060>")
-		};		
+		};
+		
 		public static List<Emote> ProPack = new List<Emote>
 		{
 			Emote.Parse(":woah:814576121541951510>"),
@@ -38,5 +53,13 @@ namespace DeepState.Data.Constants
 			Emote.Parse("<:gorepat:858524272258973696>"),
 			Emote.Parse("<:true:923420410350039060>")
 		};
+		*/
+
+		// For testing purposes, seldom-used emoji
+		public static List<Emote> TestPack = new List<Emote>
+		{
+			Emote.Parse("<:magnathonk:928158272836472872>"),
+			Emote.Parse("<:george:707391310830632990>")
+        };
     }
 }
--- DeepState/Modules/EmojiMonetizationModule.cs
+++ DeepState/Modules/EmojiMonetizationModule.cs
@@ -2,6 +2,7 @@ using DeepState.Data.Services;
 using DeepState.Constants;
 using DeepState.Modules.Preconditions;
 using EMC = DeepState.Constants.EmojiMonetizationConstants;
+using Utils = DeepState.Utilities.Utilities;
 
 using Discord.Commands;
 using Discord;
@@ -16,12 +17,12 @@ namespace DeepState.Modules
 {
     // Creates the group of commands to use
     [Group("emojipack"), Alias("ep", "emoji")]
-
     public class EmojiMonetizationModule : ModuleBase
     {
         private UserRecordsService _userRecordsService { get; set; }
         private bool _bit_active { get; set; }
         public Dictionary<string, (List<Emote> emotes, int cost)> _packs { get; set; }
+        public Dictionary<string, IRole> _packRoles { get; set; }
         
         public EmojiMonetizationModule(UserRecordsService service)
         {
@@ -30,10 +31,14 @@ namespace DeepState.Modules
 
             // TODO: Finish populating this
             // TODO: Ask Junk about command stuff
-            _packs = new() {
-                { "LibCraft Basic Pack", (EMC.BasicPack, 800) },
-                { "LibCraft Pro Pack", (EMC.ProPack, 1500)},
+
+            _packs = new() {/*
+                { "LibCraft Basic Pack", (EMC.BasicPack, 800)},
+                { "LibCraft Pro Pack", (EMC.ProPack, 1500)},*/
+                { "LibCraft Test Pack", (EMC.TestPack, 1)}
             };
+
+            _packRoles = new();
         }
 
         [Command("start")]
@@ -41,10 +46,12 @@ namespace DeepState.Modules
         public async Task StartBit()
         {
 
+            await Context.Channel.SendMessageAsync("Starting bit...");
+
             var dummyEmojiRole = await Context.Guild.CreateRoleAsync("Dummy Emoji Role", null, null, false, null);
 
             // Gets each role with a name in the pack
-            Dictionary<string, IRole> packRoles = Context.Guild.Roles
+            _packRoles = Context.Guild.Roles
                 .Where(r => _packs.Any(p => p.Key == r.Name))
                 .ToDictionary(r => r.Name, r => r);
 
@@ -53,61 +60,153 @@ namespace DeepState.Modules
                 .Where(p => !Context.Guild.Roles.Any(r => r.Name == p.Key))
                 .ToDictionary(p => p.Key, p => p.Value))
             {
-                await Context.Guild.CreateRoleAsync(pack.Key, null, null, false, null);
+                var newRole = await Context.Guild.CreateRoleAsync(pack.Key, null, null, false, null);
+                _packRoles.Add(pack.Key, newRole);
             }
 
             // Restrict all emojis
+            // TODO: uncomment when finished with testing
+            /*
             var emotes = Context.Guild.Emotes;
+            */
+            var emotes = EMC.TestPack;
             foreach (var emote in emotes)
             {
-                await Context.Guild.ModifyEmoteAsync(emote, e =>
+                await Context.Guild.ModifyEmoteAsync((GuildEmote)emote, e =>
                 {
                     e.Roles = new List<IRole>{dummyEmojiRole};
                 });
             }
 
             // Now add the pack roles to each emoji in each pack
-            foreach (var packRole in packRoles)
+            foreach (var packRole in _packRoles)
             {
-                foreach (var emote in _packs[packRole.Value.Name].emotes)
+                foreach (var emote in _packs[packRole.Key].emotes)
                 {
                     await Context.Guild.ModifyEmoteAsync((GuildEmote)emote, e =>
                     {
-                        e.Roles.Value.ToList().Add(packRole.Value);
-                    }
-                    );
+                        var roles = e.Roles.GetValueOrDefault();
+                        if (roles != null)
+                        {
+                            e.Roles = new List<IRole> { dummyEmojiRole, packRole.Value };
+                        }
+                    });
                 }
             }
 
             _bit_active = true;
+
+            await Context.Channel.SendMessageAsync("Bit successfully started!");
         }
 
-        [Command("purchase"), Alias("buy")]
-        public async Task BuyPack(string PackName)
+        [Command("end")]
+        public async Task EndBit()
         {
+            await Context.Channel.SendMessageAsync("Ending bit...");
+
+            // Clear the roles from all emojis
+            foreach(var emote in Context.Guild.Emotes)
+            {
+                await Context.Guild.ModifyEmoteAsync(emote, e =>
+                {
+                    e.Roles = new();
+                });
+            }
+        }
+
+        [Command("purchase")]
+        public async void Purchase(string input)
+        {
+            // Only allow in #bot-commands
+            if(Context.Channel.Id != /*SharedConstants.LCBotCommandsChannel*/ 707386561779597332)
+            {
+                await Context.Channel.SendMessageAsync($"Sorry, {Context.User.Username}, this command is not allowed in this channel.");
+                return;
+            }
+
+            if(!_bit_active)
+            {
+                await Context.Channel.SendMessageAsync($"We understand your enthusiasm! The LibCraft:registered: Emoji Monetization Pilot Program:tm: has ended due to public backlash, but stay tuned while we work to bring more exciting offers to you!");
+
+            }
+
             // Fetch the role, case-insensitive
             // If role doesn't exist, null
-            IRole packRole = Context.Guild.Roles.FirstOrDefault(r => r.Name.ToLower() == PackName.ToLower());
+            IRole packRole = Context.Guild.Roles.FirstOrDefault(r => r.Name.ToLower().Replace(" ", string.Empty) == input.ToLower(), null);
 
-            if(packRole == null)
+            if (packRole == null)
             {
-                await Context.Channel.SendMessageAsync("Sorry, I couldn't find that pack!");
-            }
-            else
-            {
-                double packCost = _packs[PackName].cost;
-                double senderBalance = _userRecordsService.GetUserBalance(Context.User.Id, Context.Guild.Id);
+                GuildEmote emoteBought = Context.Guild.Emotes.FirstOrDefault(r => r.Name.ToLower() == input.ToLower(), null);
                 
-                if(packCost > senderBalance)
+                if(emoteBought == null)
                 {
-                    // TODO: Say something funny here
-                    await Context.Channel.SendMessageAsync("");
+                    await Context.Channel.SendMessageAsync("Sorry, I can't find that Emoji or LibCraft:registered: Emoji Pack:tm:.");
                 }
                 else
                 {
-                    await ((IGuildUser)Context.User).AddRoleAsync(packRole);
+                    await BuyEmoji(emoteBought, Context);
                 }
-            };
+            }
+            else
+            {
+                await BuyPack(packRole, Context);
+            }
+        }
+
+        private async Task BuyPack(IRole packRole, ICommandContext Context)
+        {
+            int packCost = _packs[packRole.Name].cost;
+            double senderBalance = _userRecordsService.GetUserBalance(Context.User.Id, Context.Guild.Id);
+            string senderName = Context.User.Username;
+            Random rand = Utils.CreateSeededRandom();
+           
+                
+            if(packCost > senderBalance)
+            {
+                await Context.Channel.SendMessageAsync($"Sorry, {senderName}, but this LibCraft:registered: Emoji Pack:tm: costs {packCost} LibCoins:registered:, and you only have {senderBalance}.");
+            }
+            else
+            {
+                await ((IGuildUser)Context.User).AddRoleAsync(packRole);
+
+                _userRecordsService.Deduct(Context.User.Id, Context.Guild.Id, packCost);
+                await Context.Channel.SendMessageAsync($"Congratulations, {senderName}! You are now the proud owner of the {packRole.Name}:tm:, for the low price of just {packCost} LibCoins:registered:.\n{EMC.ThankYouMessages[rand.Next(0, EMC.ThankYouMessages.Count())]}");
+            }
+        }
+
+        private async Task BuyEmoji(GuildEmote emoteBought, ICommandContext Context)
+        {
+            if(((IGuildUser)Context.User).RoleIds.Any(r => emoteBought.RoleIds.Contains(r)))
+            {
+                await Context.Channel.SendMessageAsync($"Sorry, {Context.User.Username}, but you already own {emoteBought.Name}.");
+                return;
+            }
+
+            double senderBalance = _userRecordsService.GetUserBalance(Context.User.Id, Context.Guild.Id);
+            string senderName = Context.User.Username;
+            Random rand = Utils.CreateSeededRandom();
+
+            int emojiCost = emoteBought.Animated ? 150 : 100;
+
+            // Get the role for the emoji if it exists, otherwise create one
+            var er = _packRoles
+                .FirstOrDefault(r => r.Key.ToLower().Replace(" ", string.Empty) == emoteBought.Name.ToLower()).Value;
+
+            IRole? emoteRole = er.Equals(default(IRole)) ? null : er;
+
+            if(emoteRole != null)
+            {
+                await ((IGuildUser)Context.User).AddRoleAsync(emoteRole);
+            }
+            else
+            {
+                emoteRole = await Context.Guild.CreateRoleAsync(emoteBought.Name + " Emoji", null, null, false, null);
+                _packRoles.Add(emoteBought.Name, emoteRole);
+
+            }
+
+            _userRecordsService.Deduct(Context.User.Id, Context.Guild.Id, emojiCost);
+            await Context.Channel.SendMessageAsync($"Congratulations, {senderName}! You have bought {emoteBought.Name} for {emojiCost}.\n{EMC.ThankYouMessages[rand.Next(0, EMC.ThankYouMessages.Count())]}");
         }
     }
 }
