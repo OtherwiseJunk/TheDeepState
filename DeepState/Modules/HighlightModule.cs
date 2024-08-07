e00a19724a0a4eb7afbef610cda3a5ac33abd1a8
Wren <OtherwiseJunk@gmail.com>
2024-08-06 23:07:25 -0400

    check for existing highlights before registering
    maybe another alias we'll see if that works

--- DeepState/Modules/HighlightModule.cs
+++ DeepState/Modules/HighlightModule.cs
@@ -22,11 +22,16 @@ namespace DeepState.Modules
         [Summary("Registers the string as a highlight for you. When a message is sent to a channel you're in with the EXACT string (ignores case) the bot will send you a message with a link to the triggering message.")]
         public async Task CreateHighlight([Remainder] string triggerPhrase)
         {
-            _service.CreateHighlight(Context.Message.Author.Id, triggerPhrase);
-            await Context.Message.AddReactionAsync(new Emoji("✅"));
+            var highlights = _service.GetHighlightsForUser(Context.Message.Author.Id);
+
+            if (!highlights.Any(h => h.TriggerPhrase == triggerPhrase.ToLower()))
+            {
+                _service.CreateHighlight(Context.Message.Author.Id, triggerPhrase);
+                await Context.Message.AddReactionAsync(new Emoji("✅"));
+            }
         }
 
-        [Command("highlights") , Alias("hls")]
+        [Command("highlights") , Alias("hls", "highlight list")]
         [Summary("Lists all of your highlights.")]
         public async Task ListHighlights()
         {
